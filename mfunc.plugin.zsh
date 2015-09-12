15158346f8270538de27f55df97f140af3586d98
hlohm <hlohm@posteo.de>
2015-09-12 12:39:16 +0200

    changed functions dir

--- mfunc.plugin.zsh
+++ mfunc.plugin.zsh
@@ -12,13 +12,13 @@
 ######
 
 # make sure functions directory exists
-if [ ! -d $ZSH/functions/ ]; then
-    mkdir $ZSH/functions/
+if [ ! -d $HOME/.functions/ ]; then
+    mkdir $HOME/.functions/
     echo "mfunc init: functions directory created in $ZSH"
 fi
 
 # autoload any functions in functions directory
-[[ ! -e $ZSH/functions/* ]] && autoload $(ls $ZSH/functions/)
+[[ ! -e $HOME/.functions/* ]] && autoload $(ls $HOME/.functions/)
 
 
 #
@@ -60,12 +60,12 @@ mf_yesorno()
 }
 
 function mf_define() {
-            touch $ZSH/functions/$i
-            chmod +x $ZSH/functions/$i
+            touch $HOME/.functions/$i
+            chmod +x $HOME/.functions/$i
             echo "enter function '$i' and finish with CTRL-D"
-            cat >$ZSH/functions/$i
-            echo "new function '$i' created in $ZSH/functions"
-            source $ZSH/custom/plugins/mfunc/mfunc.plugin.zsh
+            cat >$HOME/.functions/$i
+            echo "new function '$i' created in $HOME/.functions"
+            autoload $(ls $HOME/.functions/)
             echo "function is now available"
 }
 
@@ -73,14 +73,14 @@ function mf_define() {
 function mfunc() {
 
     # berate user if no arguments given
-    # to do: interactive mode
+    # TODO: interactive mode
     if (($# == 0))
     then
         echo "usage: mfunc [function name]"
     else
-        # to do: prompt user to overwrite existing function
         for i do;
-            if [[ -e $ZSH/functions/$i ]]
+        # TODO: input sanitization
+            if [[ -e $HOME/.functions/$i ]]
             then
                 if mf_yesorno "function $i already exists, overwrite? (Y/n)"
                 then
@@ -97,15 +97,15 @@ function mfunc() {
 function rfunc() {
 
     # demand argument
-    # to do: interactive mode
+    # TODO: interactive mode
     if (($# == 0)) ; then
         echo "please name at least one function to delete";
     fi
 
-    # to do: autocompletion/wildcards
+    # TODO: autocompletion/wildcards
     for i; do
-        if [ -e $ZSH/functions/$i ]; then
-            rm $ZSH/functions/$i
+        if [ -e $HOME/.functions/$i ]; then
+            rm $HOME/.functions/$i
             echo "function $i removed"
         else
             echo "function $i not found"
@@ -116,8 +116,8 @@ function rfunc() {
 
 # list functions
 function lfunc() {
-    # to do: specific functions, wildcards
-    for f in $(ls $ZSH/functions/); do
-        echo $f "() {"; cat $ZSH/functions/$f; echo "}\n"
+    # TODO: specific functions, wildcards
+    for f in $(ls $HOME/.functions/); do
+        echo $f "() {"; cat $HOME/.functions/$f; echo "}\n"
     done
 }
